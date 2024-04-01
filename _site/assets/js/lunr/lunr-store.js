33912f97069bc9d7ffdddfb7940c9d8c4d10bebe
aff11001 <44692385+aff11001@users.noreply.github.com>
2024-04-02 06:41:13 +0800

    update

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,5 +1,11 @@
 var store = [{
         "title": "Perception workshop",
+        "excerpt":"   ","categories": ["Helaine_Coaching"],
+        "tags": [],
+        "url": "/helaine_coaching/2024/03/30/perception.html",
+        "teaser": null
+      },{
+        "title": "Perception workshop",
         "excerpt":"   ","categories": [],
         "tags": [],
         "url": "/2024/03/30/perception.html",
